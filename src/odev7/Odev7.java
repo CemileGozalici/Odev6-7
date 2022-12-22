5ca6332941ea332c2c38bd8b62c5a6f8ac9b8ba4
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2022-12-22 15:45:26 +0300

    Update Odev7.java
    vize adlı değişken vize_notu olarak değiştirildi. Hatalı giriş kontrolü eklendi.

--- src/odev7/Odev7.java
+++ src/odev7/Odev7.java
@@ -6,47 +6,55 @@ public class Odev7 {
 
     public static void main(String[] args) {
 
-        int vize, final_notu;
+        int vize_notu, final_notu; //vize adlı değişken vize_notu olarak değiştirildi.
         double sonuc;
 
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Vize Notunuzu Giriniz: ");
-        vize = scan.nextInt();
+        vize_notu = scan.nextInt();
 
         System.out.print("Final Notunuzu Giriniz: ");
         final_notu = scan.nextInt();
 
-        sonuc = (vize * 0.40 + final_notu * 0.60);
-        System.out.println("Ders Notunuz: " + sonuc);
-
-        if (sonuc >= 95) {
-            System.out.println("Harf Notunuz A1 =>Başarılı");
-
-        } else if (sonuc >= 90) {
-            System.out.println("Harf Notunuz A2 =>Başarılı");
-
-        } else if (sonuc >= 85) {
-            System.out.println("Harf Notunuz A3 =>Başarılı");
-
-        } else if (sonuc >= 70) {
-            System.out.println("Harf Notunuz B =>Başarılı");
-
-        } else if (sonuc >= 55) {
-            System.out.println("Harf Notunuz C =>Başarılı");
-
-        } else if (sonuc >= 40) {
-            System.out.println("Harf Notunuz D =>Şartlı Başarılı");
-
-        } else if (sonuc >= 35) {
-            System.out.println("Harf Notunuz F1 =>Başarısız");
-
-        } else if (sonuc >= 0) {
-            System.out.println("Harf Notunuz F2 =>Başarısız");
-
-        } else {
-            System.out.println("Harf Notunuz F3 Devamsızlıktan kaldınız!");
-        }
+        if ((vize_notu>100)||(final_notu>100)){  //hatalı giriş kontrolü eklendi.
+            System.out.println("Hatalı not girişi yaptınız!");
+        }else{
+            sonuc=(vize_notu*0.40+final_notu*0.60);
+            System.out.println("Ders Notunuz: "+sonuc);
+         
+            if (sonuc>85) {
+         
+                System.out.println("Harf Notunuz AA");
+            }
+            else if (sonuc<85 && sonuc>80) {
+         
+             System.out.println("Harf Notunuz BA");
+            }
+            else if (sonuc<80 && sonuc>75) {
+         
+            System.out.println("Harf Notunuz BB");
+            }
+            else if (sonuc<75 && sonuc>65) {
+         
+             System.out.println("Harf Notunuz CB");
+            }
+            else if (sonuc<65 && sonuc>60) {
+            
+             System.out.println("Harf Notunuz CC");
+            }
+            else if (sonuc<60&& sonuc>55) {
+         
+             System.out.println("Harf Notunuz CD");
+             }
+            else if (sonuc<55 && sonuc>50) {
+         
+             System.out.println("Harf Notunuz DD");
+            }
+            else{
+              System.out.println("Harf Notunuz FF Basarisiz Oldunuz!");
+            }
+     }
     }
 
 }
